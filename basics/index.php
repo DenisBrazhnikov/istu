b2dfb5a0f325a840ba4f72dd5942cc85779245ca
den <den@test.com>
2024-10-17 10:13:16 +0300

    Update headers

--- basics/index.php
+++ basics/index.php
@@ -2,5 +2,5 @@
 
 setcookie('session', md5('session'), 0, '', '', false, false);
 
-header('SERVER', '2.4.59');
-header('X-POWERED-BY', 'PHP 8.1.30');
+header('SERVER: 2.4.59', true);
+header('X-POWERED-BY: PHP 8.1.30', true);
