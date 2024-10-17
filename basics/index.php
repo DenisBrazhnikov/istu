a7e903121de446248c725e4b41104d615a276d73
den <den@test.com>
2024-10-17 10:49:14 +0300

    implement basic division calc without zero validation

--- basics/index.php
+++ basics/index.php
@@ -1,19 +1,20 @@
 <?php
 
-/*setcookie('session', md5('session'), 0, '', '', false, false);
+setcookie('session', md5('session'), 0, '', '', false, false);
 
 header('X-SERVER: 2.4.59');
-header('X-POWERED-BY: PHP 8.1.30');*/
-
-//phpinfo();
+header('X-POWERED-BY: PHP 8.1.30');
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+if(isset($_POST['a']) && isset($_POST['b'])) {
+    $a = intval($_POST['a']);
+    $b = intval($_POST['b']);
+    $result = $a/$b;
+}
 
-echo 2/0;
-/*
 echo '
 <html lang="en">
 <head>
@@ -21,9 +22,17 @@ echo '
 </head>
 
 <body>
+    <h2>Calc (Division operation)</h2>
     <form method="post">
-    
+        <input type="text" placeholder="Value 1" name="a">
+        <br>
+        <input type="text" placeholder="Value 1" name="b">
+        <br>
+        <button type="submit">Calculate</button>
+        <br>
+        <br>
+        Result: '. isset($result) ? $result : 'No result yet' .'
     </form>
 </body>
 </html>
-';*/
+';
