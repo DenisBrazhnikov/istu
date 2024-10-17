61621441eaaa89215ddfb5d5912fcb44a31bc3ca
den <den@test.com>
2024-10-17 10:29:11 +0300

    Division by zero

--- basics/index.php
+++ basics/index.php
@@ -5,7 +5,9 @@ setcookie('session', md5('session'), 0, '', '', false, false);
 header('X-SERVER: 2.4.59');
 header('X-POWERED-BY: PHP 8.1.30');
 
-echo 2/0;
+phpinfo();
+
+//echo 2/0;
 
 echo '
 <html lang="en">
